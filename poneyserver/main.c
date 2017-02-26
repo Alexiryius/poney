30b41501553860c95dceda8f8731cd8b55c7f86b
utilisateur <utilisateur>
2017-02-26 21:07:37 +0100

    reponse serveur

--- poneyserver/main.c
+++ poneyserver/main.c
@@ -42,19 +42,6 @@ void daryl_signal(void){
 	}
 }
 
-char* recupLigne(char* buf){
-	char* retour = malloc(100);
-	//char* r_realloc;
-	int compt =0 ;
-	while(buf[compt] != '\r' && buf[compt+1]!= '\n'){
-		retour[compt] = buf[compt];
-		compt++;
-	}
-	return retour;
-}
-
-//faire un strstok
-
 
 int main(int argc, char **argv)
 {
@@ -62,7 +49,9 @@ int main(int argc, char **argv)
 	int socket_client;
 	char buff[256];
 	int i = 0;
-	char ligneHein[300];
+	int tailleContenu;
+	char *mot;
+	char *message1;
 
 	FILE *fsc; //pour utiliser avec fgets, fprintf...
 
@@ -89,18 +78,46 @@ int main(int argc, char **argv)
 			perror("erreur de fork");
 			return -1;
 		}
-
-		else if(fok == 0){
-		//on est fils
-		fgets(buff, 256, fsc);
 		
-
-			while(fgets(buff, 256, fsc)){
-				if(strncmp(buff,"\r\n",2)){break;
+		//on est fils
+		else if(fok == 0){
+			fgets(buff, 256, fsc);
+			tailleContenu = strlen(buff);
+			//verification de la chaine, doit être de 3 mots, GET / xxx / HTTP/M.m
+			i = 1;
+			mot = strtok(buff, " ");
+			while(mot != NULL && i <= 3){
+				//on verifie que le 1er mot est GET, si pas on sort du while
+				if(i == 1 && (strncmp(mot, "GET", strlen(mot))) != 0){
+					printf("1er mot pas bon\n");
+					i = 5;
 				}
-				fprintf(stdout, "%s", buff);
+				//on verifie que le 3eme mot est HTTP/1.0 ou HTTP/1.1, si pas on sort du while
+				if(i == 3 && ((strncmp(mot, "HTTP/1.1\r\n", strlen(mot))) != 0 && (strncmp(mot, "HTTP/1.0\r\n", strlen(mot))) != 0)){
+					printf("3eme mot pas bon\n");
+					i = 5;
+				}
+				mot = strtok(NULL, " ");
+				i++;
 			}
-			printf("ce qui est récupéré %s", ligneHein);
+		
+		
+			//on ne fait les traitements suivants que si la premiere ligne est correcte
+			if(i == 4){
+				message1 = "HTTP/1.1 200 OK\r\nConnection: close\r\nContent-Length: 100\r\n";
+				fprintf(fsc, "%s" , message1);
+				while(fgets(buff, 256, fsc)){
+					tailleContenu += strlen(buff);
+					//le if permet d'ignorer les lignes envoyees par le client tant qu'il ne s'agit pas d'une ligne vide
+					if(strncmp(buff,"\r\n",2)){
+						break;
+					}
+				}
+				fprintf(fsc, "HTTP/1.1 200 OK\r\nConnection: close\r\nContent-Length: %d\r\n", tailleContenu);
+			}
+			else{
+				fprintf(fsc, "HTTP/1.1 400 Bad Request\r\nConnection: close\r\nContent-Length: 17\r\n\r\n400 Bad request\r\n");
+			}
 			exit(0);
 		}
 		else{
@@ -117,7 +134,4 @@ int main(int argc, char **argv)
 	}
 	printf("Need an advice?\n");
 	return 0;
-}
-
-//char *fgets(char *s, int size, FILE *stream)
-//char *fprintf(FILE *stream, const char *format,...) 
+} 
